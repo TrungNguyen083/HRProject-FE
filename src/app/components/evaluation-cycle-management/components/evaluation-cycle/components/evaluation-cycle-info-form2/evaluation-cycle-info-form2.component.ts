f8d884ebbd79d90fa4e5d219fc0dd94feae54413
KhaiLam-1004 <khailam1029@gmail.com>
2024-09-03 18:08:33 +0700

    FrontEnd Create cycle step1 and step2

---
+++ src/app/components/evaluation-cycle-management/components/evaluation-cycle/components/evaluation-cycle-info-form2/evaluation-cycle-info-form2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evaluation-cycle-info-form2',
+  templateUrl: './evaluation-cycle-info-form2.component.html',
+  styleUrls: ['./evaluation-cycle-info-form2.component.scss']
+})
+export class EvaluationCycleInfoForm2Component {
+
+}
